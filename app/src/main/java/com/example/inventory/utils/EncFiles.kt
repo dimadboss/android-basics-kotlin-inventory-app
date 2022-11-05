28873626b6374c437df32adb9e5c7babcf1c76ac
D Drozdov <ds-drozdov@yandex.ru>
2022-11-05 21:35:06 +0300

    up security libs & rework encrypting files

--- app/src/main/java/com/example/inventory/utils/EncFiles.kt
+++ app/src/main/java/com/example/inventory/utils/EncFiles.kt
@@ -2,44 +2,102 @@ package com.example.inventory.utils
 
 import android.content.Context
 import android.net.Uri
+import android.util.Log
+import androidx.core.net.toUri
 import androidx.security.crypto.EncryptedFile
-import androidx.security.crypto.MasterKeys
+import androidx.security.crypto.MasterKey
 import com.example.inventory.data.Item
+import com.example.inventory.data.getTmpFileName
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.io.File
 import java.io.FileOutputStream
-import java.time.Instant
-import java.time.format.DateTimeFormatter
+import java.nio.charset.StandardCharsets
 
 class EncFiles {
-    private val masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+    private var masterKey: MasterKey? = null
+
+    private fun getMasterKey(ctx: Context): MasterKey {
+        if (masterKey == null) {
+            masterKey = MasterKey.Builder(ctx)
+                .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+                .build()
+        }
+
+        return masterKey!!
+    }
 
     fun encryptData(
         ctx: Context,
         uri: Uri,
         item: Item,
     ) {
-        val name = DateTimeFormatter.ISO_INSTANT.format(Instant.now())
+
+        val name = item.getTmpFileName()
         val tmp = File(ctx.cacheDir, name)
 
+        if (tmp.exists()) {
+            tmp.delete()
+        }
+
         val encryptedFile = EncryptedFile.Builder(
-            tmp,
             ctx,
-            masterKeyAlias,
+            tmp,
+            getMasterKey(ctx),
             EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB,
         ).build()
 
+        var bytes = Json.encodeToString(item).toByteArray(StandardCharsets.UTF_8)
+
+        Log.i("step 1", bytes.decodeToString())
+
         encryptedFile.openFileOutput().apply {
-            write(Json.encodeToString(item).toByteArray())
+            write(bytes)
             flush()
             close()
         }
 
+        bytes = tmp.readBytes()
+
         ctx.contentResolver.openFileDescriptor(uri, "w")?.use {
-            FileOutputStream(it.fileDescriptor).use {
-                it.write(tmp.readBytes())
+            FileOutputStream(it.fileDescriptor).use { o ->
+                o.write(bytes)
+                o.flush()
             }
         }
+
+        Log.i("step 2", bytes.decodeToString())
+
+        tmp.delete()
+    }
+
+
+    fun decryptDate(ctx: Context, uri: Uri): Item {
+        val tmp = File(
+            ctx.cacheDir,
+            uri.getDisplayName(ctx.contentResolver),
+        )
+
+        ctx.contentResolver.openInputStream(uri)?.use { i ->
+            ctx.contentResolver.openOutputStream(tmp.toUri())?.use { o ->
+                i.copyTo(o)
+            }
+        }
+
+        val encryptedFile = EncryptedFile.Builder(
+            ctx,
+            tmp,
+            getMasterKey(ctx),
+            EncryptedFile.FileEncryptionScheme.AES256_GCM_HKDF_4KB
+        ).build()
+
+        val bytesStr = encryptedFile.openFileInput().readBytes().decodeToString()
+
+        Log.i("step 3", bytesStr)
+
+        tmp.delete()
+
+        return Json.decodeFromString(bytesStr)
     }
 }
